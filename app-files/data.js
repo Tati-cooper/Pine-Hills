92c967487850a5c5939929198ed7dff84faf8171
Tati-cooper <141407507+Tati-cooper@users.noreply.github.com>
2024-05-10 14:27:17 +0500

    upload

---
+++ app-files/data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hall",
+      "name": "Hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -2.795894300623809,
+        "pitch": 0.011563651453737833,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8886953805321234,
+          "pitch": 0.042568075132358274,
+          "rotation": 4.71238898038469,
+          "target": "4-shkaf"
+        },
+        {
+          "yaw": -2.008384206933888,
+          "pitch": 0.03636415073482269,
+          "rotation": 1.5707963267948966,
+          "target": "1-kamin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kamin",
+      "name": "Kamin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.9175606441142232,
+        "pitch": 0.011243985623227815,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3236281278307622,
+          "pitch": 0.027721543953493466,
+          "rotation": 4.71238898038469,
+          "target": "0-hall"
+        },
+        {
+          "yaw": 1.5834648301332255,
+          "pitch": 0.1728485738435417,
+          "rotation": 0,
+          "target": "3-lestnica"
+        },
+        {
+          "yaw": 2.6898314469463784,
+          "pitch": 0.4271282912800771,
+          "rotation": 3.141592653589793,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": -1.9505909270861146,
+          "pitch": 0.43105323232467363,
+          "rotation": 3.141592653589793,
+          "target": "5-tv-zona"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -2.001935809231165,
+        "pitch": 0.012118535097471295,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0525275387458244,
+          "pitch": 0.20957748642678808,
+          "rotation": 0,
+          "target": "5-tv-zona"
+        },
+        {
+          "yaw": -0.2916750098278431,
+          "pitch": -0.14277719586769777,
+          "rotation": 0,
+          "target": "1-kamin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lestnica",
+      "name": "Lestnica",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -2.775064377603215,
+        "pitch": -0.0018739976038624206,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.38756954629033835,
+          "pitch": 0.38302655676007014,
+          "rotation": 0,
+          "target": "4-shkaf"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-shkaf",
+      "name": "Shkaf",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.2190453544698912,
+        "pitch": -0.003747995207742605,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2234816141351121,
+          "pitch": 0.26494213719901616,
+          "rotation": 0,
+          "target": "1-kamin"
+        },
+        {
+          "yaw": -0.5530656815445383,
+          "pitch": 0.09277621741559017,
+          "rotation": 1.5707963267948966,
+          "target": "0-hall"
+        },
+        {
+          "yaw": -2.907786094206397,
+          "pitch": 0.3830987463380815,
+          "rotation": 0,
+          "target": "3-lestnica"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-tv-zona",
+      "name": "TV zona",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.2166969285875915,
+        "pitch": -0.0028496273824494267,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6605555198210347,
+          "pitch": 0.1927641482353657,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": 1.0605717760065723,
+          "pitch": -0.21938662911149898,
+          "rotation": 6.283185307179586,
+          "target": "1-kamin"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
